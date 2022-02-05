742f1e3158b1a462ba3668a1368b04feb6fe895d
zedxpace <52217313+zedxpace@users.noreply.github.com>
2022-02-05 12:25:30 +0530

    Create set_text_in_TextView.java

---
+++ TextView/set_text_in_TextView.java
@@ -0,0 +1,6 @@
+//replace player_turn with the id of your TextView 
+String player_name = "zedxpace";
+TextView status = findViewById(R.id.player_turn);
+//Player name is the String variable whose String will be displayed on TextView
+status.setText(player_name);
+view raw
